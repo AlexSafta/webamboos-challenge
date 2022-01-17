c56be5596fb5fd00afcb9bea11d3fb6f2324582d
Alexandru Safta <alexsafta98@gmail.com>
2022-01-18 01:05:13 +0200

    added <Load more> button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { FaPlus, FaMinus } from 'react-icons/fa';
+import React from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
-import '../css/Card.css';
+import "../css/Card.css";
 
-const Card = ({ title, message, expanded, handleOnToggle }) => {
+const Card = ({ title, message, expanded, handleOnToggle, isButton, handleOnClick }) => {
   return (
-    <div className='faq-card'>
-      <div className='faq-title' onClick={handleOnToggle}>
-        <p className='title'>{title}</p>
-        <p className='icon'>
-          {
-            expanded ? <FaMinus /> : <FaPlus />
-          }
+    <div className={`faq-card ${isButton ? "red-backgr" : ""}`} onClick={() => isButton && handleOnClick()} >
+      <div className={`faq-title ${isButton ? "red-title" : ""}`} onClick={handleOnToggle}>
+        <p className="title">{title}</p>
+        <p className="icon">
+          {isButton ? <FaPlus /> : expanded ? <FaMinus /> : <FaPlus />}
         </p>
       </div>
-      <div className='message' style={!expanded ? {display: 'none'} : {}}>
-        <p>{message}</p>
-      </div>
+      {!isButton && (
+        <div className="message" style={!expanded ? { display: "none" } : {}}>
+          <p>{message}</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -7,13 +7,27 @@ import "../css/FAQ.css";
 import background from "../assets/faq-backgr.png";
 import person from "../assets/faqs.png";
 
+const MIN_CARDS = 10;
+
 const FAQ = () => {
+  const [indexData, setIndexData] = useState(0);
+  const [canLoadMore, setCanLoadMore] = useState(false);
   const [dataLoaded, setDataLoaded] = useState(false);
   const [data, setData] = useState(null);
 
   useEffect(() => {
     if (!dataLoaded || !data) fetchData();
   }, []);
+
+  useEffect(() => {
+    if (dataLoaded && !!data && indexData === 0) {
+      let numOfItems = data.length;
+      let value = MIN_CARDS < numOfItems ? MIN_CARDS : numOfItems;
+
+      setCanLoadMore(MIN_CARDS < numOfItems);
+      setIndexData(value);
+    }
+  }, [data, dataLoaded]);
 
   const fetchData = async () => {
     const response = await fetch(DATA_URL);
@@ -32,10 +46,20 @@ const FAQ = () => {
     let newExpArr = [...data];
     newExpArr[index].expanded = !newExpArr[index].expanded;
 
-    console.log(newExpArr);
-
     setData(newExpArr);
   }
+
+  const handleShowMore = () => {
+    let itemsLeft = data.length - indexData;
+    if (itemsLeft > 0) {
+      setCanLoadMore(true);
+      if (itemsLeft > MIN_CARDS) setIndexData(indexData + MIN_CARDS);
+      else {
+        setIndexData(indexData + itemsLeft);
+        if (indexData + itemsLeft === data.length) setCanLoadMore(false);
+      }
+    } else setCanLoadMore(false);
+  };
 
   return (
     <div className="faq">
@@ -51,15 +75,29 @@ const FAQ = () => {
       </div>
       <div className="questions">
       {
-        dataLoaded && !!data && data.map((elem, index) => (
-          <Card
+        dataLoaded && !!data && data.map((elem, index) => {
+          return index < indexData ? (
+            <Card
             key={index}
             title={elem.title}
             message={elem.message}
             expanded={elem.expanded}
             handleOnToggle={() => handleOnToggle(index)}
+            isButton={false}
           />
-        ))
+          ) : null;
+        })
+      }
+      {
+        canLoadMore && !!data && (
+          <div className="faq-card load-more-div">
+            <Card
+              title={'LOAD MORE'}
+              isButton={true}
+              handleOnClick={handleShowMore}
+            />
+          </div>
+        )
       }
       </div>
     </div>
